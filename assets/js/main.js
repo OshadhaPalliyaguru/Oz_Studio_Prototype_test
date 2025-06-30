bc411bbe9b00e20b2b9a57e38f6380f5936d487a
Oshadha <hpgmoshadhapalliyaguru@gmail.com>
2025-06-30 15:50:35 +0530

    Third commit

--- assets/js/main.js
+++ assets/js/main.js
@@ -147,23 +147,6 @@ sr.reveal(`.home__data, .home__img,
     ]
   };
 
-  // for (let key in iconLoops) {
-  //   let index = 0;
-  //   const element = document.getElementById(key);
-  //   element.style.backgroundImage = `url(${iconLoops[key][index]})`;
-
-  //   setInterval(() => {
-  //     element.style.opacity = 0;
-  //     setTimeout(() => {
-  //       index = (index + 1) % iconLoops[key].length;
-  //       element.style.backgroundImage = `url(${iconLoops[key][index]})`;
-  //       element.style.opacity = 1;
-  //     }, 500);
-  //   }, 1500);
-  // }
-
-  //Service Section Icons Chage Effect
-
 
 
   for (let key in iconLoops) {
@@ -197,50 +180,6 @@ function openWhatsApp(serviceName) {
   const waLink = `https://wa.me/${phoneNumber}?text=${encodedMessage}`;
   window.open(waLink, '_blank');
 }
-
-
-
-
-
-//email js Contact us now
-
-
-//  window.onload = function () {
-//     emailjs.init("mvWd6lGac7-wv9iRh"); 
-
-//     document.getElementById("Form").addEventListener("submit", function (e) {
-//       e.preventDefault();
-
-//       emailjs.sendForm("service_nkpzk3o", "template_fvgi4av", this)
-//         .then(() => {
-//           Swal.fire({
-//             icon: 'success',
-//             title: 'Message Sent!',
-//             text: '✅ We received your message. We will reply soon.',
-//             background: isDarkMode() ? '#1e1e1e' : '#ffffff',
-//             color: isDarkMode() ? '#ffffff' : '#d32f2f',
-//             confirmButtonColor: '#d32f2f'
-//           });
-//           this.reset();
-//         }, (error) => {
-//           Swal.fire({
-//             icon: 'error',
-//             title: 'Oops!',
-//             text: '❌ Failed to send message. Please try again later.',
-//             background: isDarkMode() ? '#1e1e1e' : '#ffffff',
-//             color: isDarkMode() ? '#ffffff' : '#d32f2f',
-//             confirmButtonColor: '#d32f2f'
-//           });
-//           console.error("EmailJS error:", error);
-//         });
-//     });
-
-//     function isDarkMode() {
-//       return document.documentElement.classList.contains("dark") || 
-//              window.matchMedia("(prefers-color-scheme: dark)").matches;
-//     }
-//   };
-
 
 
 function isValidEmail(email) {
@@ -349,3 +288,57 @@ function startWebsite() {
 
 
 
+  /*==================== SCROLL FUNCTION ====================*/
+
+  if ('scrollRestoration' in history) {
+    history.scrollRestoration = 'manual';
+  }
+
+  window.onbeforeunload = function () {
+    window.scrollTo(0, 0);
+  };
+
+
+
+    /*==================== Privacy Policy====================*/
+
+    function showPrivacyPolicy() {
+    Swal.fire({
+      title: '<img src="logos/oz studios logo4.png" style="width:100px;margin-bottom:5px;">',
+      html: `
+        <h3 style="margin-bottom:10px;">Privacy Policy</h3>
+        <p style="text-align:left; max-height:300px; overflow-y:auto;">
+          We collect basic information such as name, email, and contact details when users interact with our services. 
+          This data is used strictly to enhance user experience and is never sold or shared with third parties. 
+          All data is securely stored and you may request deletion at any time. 
+          For questions, 
+        </p>
+        <p>contact us at:</p> <h4>oz.dev.info@gmail.com</h4>
+      `,
+      confirmButtonText: 'OK',
+      confirmButtonColor: '#d33',
+      width: 600
+    });
+  }
+
+
+      /*==================== Terms and COnditions ====================*/
+
+
+  function showTerms() {
+    Swal.fire({
+      title: '<img src="logos/oz studios logo4.png" style="width:100px;margin-bottom:5px;">',
+      html: `
+        <h3 style="margin-bottom:10px;">Terms of Service</h3>
+        <p style="text-align:left; max-height:300px; overflow-y:auto;">
+          By using our services, you agree not to misuse or violate our platform. 
+          All content, design, and development work is protected and cannot be copied or reused without permission. 
+          We reserve the right to update terms at any time. 
+          Continued use of our services means you accept the latest terms.
+        </p>
+      `,
+      confirmButtonText: 'OK',
+      confirmButtonColor: '#d33',
+      width: 600
+    });
+  }
